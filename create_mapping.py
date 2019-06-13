7957b298a0ff51818b5815ab17dd6d156f6a2f57
Harm Delva <harm.delva@gmail.com>
2019-06-13 15:16:23 +0200

    Update the mapping file

---
+++ create_mapping.py
@@ -0,0 +1,58 @@
+import json
+import csv
+from collections import defaultdict
+
+def add_name(entries):
+	entries.append({
+		"osm_key": "name",
+		"predicate": "osm:name"
+	})
+
+def add_maxspeed(entries):
+	mapping = {}
+
+	with open('speeds.json') as f:
+		speeds = json.load(f)
+
+	for entry in speeds["data"]:
+		value = entry["value"]
+		try:
+			parsed_value = float(value)
+			mapping[value] = parsed_value
+		except:
+			if len(value.split('mph')) == 2:
+				    parsed_value = int(round(float(value.split('mph')[0]) * 1.609344))
+				    mapping[value] = parsed_value
+
+	entries.append({
+		"osm_key": "maxspeed",
+		"predicate": "osm:maxspeed",
+		"mapping": mapping
+	})
+
+def read_entities():
+	entity_keys = defaultdict(dict)
+
+	with open('entities.csv') as csvfile:
+		next(csvfile)
+		spamreader = csv.reader(csvfile)
+		for row in spamreader:
+			key = row[1]
+			entity_keys[key]["osm_key"] = key.strip()
+			entity_keys[key]["predicate"] = row[2].strip()
+
+			value = row[0][len(key) + 1:]
+			mapped_value = row[3].strip()
+
+			if 'mapping' not in entity_keys[key]:
+				entity_keys[key]["mapping"] = {}
+			
+			entity_keys[key]["mapping"][value] = mapped_value
+
+	return list(entity_keys.values())
+
+result = read_entities()
+add_maxspeed(result)
+add_name(result)
+
+print(json.dumps(result, indent=2, sort_keys=True))
